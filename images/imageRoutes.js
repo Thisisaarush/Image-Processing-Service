90df93a2dbbd30ebbc455337a18472dcbb79ee16
Aarush Tanwar <tanwaraarush007@gmail.com>
2024-09-18 21:37:45 +0530

    Add rate limiting for image uploads and transformations

--- images/imageRoutes.js
+++ images/imageRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const authMiddleware = require("../auth/authMiddleware")
 const multer = require("multer")
+const redisClient = require("../configs/redis")
+const rateLimit = require("express-rate-limit")
+const RateLimitRedisStore = require("rate-limit-redis")
+
 const {
   uploadImageController,
   transformImageController,
@@ -12,16 +16,42 @@ const {
 const router = express.Router()
 const upload = multer({ dest: "uploads/" })
 
+// Rate limiting for image transformations
+const transformLimiter = rateLimit({
+  store: new RateLimitRedisStore({
+    client: redisClient,
+  }),
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to few transform requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+})
+
+// Rate limiting for image uploads
+const uploadLimiter = rateLimit({
+  store: new RateLimitRedisStore({
+    client: redisClient,
+  }),
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20, // Limit each IP to few upload requests per windowMs
+  message: "Too many upload requests from this IP, please try again later.",
+})
+
 // Routes for images
 router.get("/", authMiddleware, listImagesController)
 router.get("/:id", getImageController)
 router.post(
   "/upload",
   authMiddleware,
+  uploadLimiter,
   upload.single("image"),
   uploadImageController
 )
-router.post("/transform/:id", authMiddleware, transformImageController)
+router.post(
+  "/transform/:id",
+  authMiddleware,
+  transformLimiter,
+  transformImageController
+)
 router.delete("/:id", authMiddleware, deleteImageController)
 
 module.exports = router
